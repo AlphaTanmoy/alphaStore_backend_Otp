be4aa26a22e00a97d9cb32540539ab9a37c6675c
AlphaTanmoy <dtanmoy169@gmail.com>
2024-11-10 10:33:43 +0530

    main file uploaded

--- src/main/kotlin/com/AlphaStore/Otp/OtpApplication.kt
+++ src/main/kotlin/com/AlphaStore/Otp/OtpApplication.kt
@@ -2,8 +2,12 @@ package com.alphaStore.Otp
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient
+import org.springframework.scheduling.annotation.EnableAsync
 
 @SpringBootApplication
+@EnableDiscoveryClient
+@EnableAsync
 class OtpApplication
 
 fun main(args: Array<String>) {
